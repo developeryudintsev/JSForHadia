163754d181a237acfaf1c86b5ff13370661f1e80
login <kakvospitat@gmail.com>
2021-07-02 11:34:19 +0300

    lesson4-concatenation

--- index.js
+++ index.js
@@ -1,4 +1,23 @@
 // lesson 3 :console.log
-let Nadya = 'Nadya is cool girl';
+// let Nadya = 'Nadya is cool girl';
 // alert(Nadya)
-console.log(Nadya)
+// console.log(Nadya)
+
+//=======================================================================================
+// lesson 4 : concatenation
+let a = 1;
+let b = 2;
+let c = 3;
+console.log(a + b - c)//sum of numbers
+
+let d = 'Misha is';
+let space = ' '
+let e = 'cool boy';
+console.log(d + space + e)//concatenation of strings
+
+let f = '2'
+let g = '3'
+let h = 'a'
+let i = 'b'
+console.log(f + g)
+console.log(h + i)
