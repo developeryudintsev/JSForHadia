f8674be9a862a09d71770872c4035b9c3d9a232d
login <kakvospitat@gmail.com>
2021-07-15 13:12:29 +0300

    lesson-5 Array

--- index.js
+++ index.js
@@ -5,19 +5,26 @@
 
 //=======================================================================================
 // lesson 4 : concatenation
-let a = 1;
-let b = 2;
-let c = 3;
-console.log(a + b - c)//sum of numbers
+// let a = 1;
+// let b = 2;
+// let c = 3;
+// console.log(a + b - c)//sum of numbers
 
-let d = 'Misha is';
-let space = ' '
-let e = 'cool boy';
-console.log(d + space + e)//concatenation of strings
+// let d = 'Misha is';
+// let space = ' '
+// let e = 'cool boy';
+// console.log(d + space + e)//concatenation of strings
 
-let f = '2'
-let g = '3'
-let h = 'a'
-let i = 'b'
-console.log(f + g)
-console.log(h + i)
+// let f = '2'
+// let g = '3'
+// let h = 'a'
+// let i = 'b'
+// console.log(f + g)
+// console.log(h + i)
+
+//=======================================================================================
+//lesson 5 : Array
+let Array = [1, 2, 3, 'Misha', 'Nadya']
+let Array2 = [1, 'string']
+let bag = []
+console.log(Array[4])
